0b4f4a9bf7feefe97e430807c87d4eb1bd7b4d3b
xor_xe <bedauri.it@gmail.com>
2025-09-19 00:04:18 +0400

    opacity

--- hyprland/config.nix
+++ hyprland/config.nix
@@ -15,8 +15,28 @@
     enable = true;
     xwayland.enable = true;
 
+
     settings = {
       "$mod" = "SUPER";
+       decoration = {
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          new_optimizations = true; # Better performance :cite[3]
+        };
+        dim_inactive = false;
+        active_opacity = 0.95;
+        inactive_opacity = 0.8;
+        fullscreen_opacity = 1.0;
+      };
+
+       windowrulev2 = [
+        "opacity 0.8 override 0.8 override,class:^(ghostty)$"
+        "opacity 0.9 0.7,floating:1"
+        "noblur,class:^(firefox)$"
+      ];
+
     };
   };
 }

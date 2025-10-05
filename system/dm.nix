de681ff7ddb2d20123d7b3f561ca2cd4dbef0282
xor_xe <bedauri.it@gmail.com>
2025-10-06 01:37:15 +0400

    mod ly

--- system/dm.nix
+++ system/dm.nix
@@ -1,4 +1,10 @@
 {...}: {
   # Display Manager - ly
   services.displayManager.ly.enable = true;
+  
+  services.displayManager.ly.settings = {
+    animation =  "matrix";
+    bigclock = true;
+    bigclock_12hr = true;
+  };
 }

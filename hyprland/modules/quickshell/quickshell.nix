34cb4353f0b3cbd07a1ecccb94d36f8d7d720a50
xor_xe <bedauri.it@gmail.com>
2025-10-07 12:40:52 +0400

    config

--- hyprland/modules/quickshell/quickshell.nix
+++ hyprland/modules/quickshell/quickshell.nix
@@ -1,5 +1,7 @@
 {...}:{
 
+  # it is run from dotfiles for now, but should be changed it to be run from home
+
   home.file.".config/quickshell/shell.qml".source = ./configs/main.qml;
   home.file.".config/quickshell/notifications/SoundNotification.qml".source = ./configs/notifications/SoundNotification.qml;
   home.file.".config/quickshell/notifications/BrightnessNotification.qml".source = ./configs/notifications/BrightnessNotification.qml;
